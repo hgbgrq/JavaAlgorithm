81389bec8f659eca6836defa52ea5575ece0cef7
hgb <hgbgrq@gmail.com>
2022-10-03 16:17:16 +0900

    코딩 테스트 1, 2

---
+++ src/codingTest/KAKAO2022TECHINTERNSHIP2.java
@@ -0,0 +1,80 @@
+package codingTest;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class KAKAO2022TECHINTERNSHIP2 {
+    public int solution(int[] queue1, int[] queue2) {
+    
+        int sum = 0;
+        
+        for(int i = 0; i < queue1.length; i++) {
+        	sum = sum + queue1[i] + queue2[i];
+        }
+        
+        if(sum % 2 == 1) {
+        	return -1;
+        }
+        
+        long max = (queue1.length + queue2.length ) * 2;
+
+        Queue<Integer> queue1List = makeList(queue1);
+        Queue<Integer> queue2List = makeList(queue2);
+        
+        long sumQueue1 = sumQueue(queue1List);
+        long sumQeueu2 = sumQueue(queue2List);
+        
+        long count = 0;
+        while(true) {
+        	
+          	if(max < count) {
+        		return -1;
+        	}
+        	
+        	if(sumQueue1 == sumQeueu2) {
+        		break;
+        	}
+        	
+        	else if (sumQueue1 > sumQeueu2) {
+        		int chgNum = queue1List.peek();
+        		sumQueue1 -= chgNum;
+                sumQeueu2 += chgNum;
+        		queue2List.add(chgNum);
+        		queue1List.poll();
+        	}
+        	else {
+        		int chgNum = queue2List.peek();
+                sumQeueu2 -= chgNum;
+                sumQueue1 += chgNum;
+        		queue1List.add(chgNum);
+        		queue1List.poll();
+        	}
+     
+        	if(sumQueue1 == 0 || sumQeueu2 == 0) {
+        		return -1;
+        	}
+        	
+        	
+        	count++;
+        }
+        return (int) count;
+    }
+    
+    private Queue<Integer> makeList(int[] queue){
+    	Queue<Integer> resultList = new LinkedList<>();
+    	for(Integer queue1Num: queue) {
+    		resultList.add(queue1Num);
+        }
+    	return resultList;
+    }
+    
+    private long sumQueue(Queue<Integer> queueList) {
+    	long sum = 0;
+    	
+    	for(Integer num: queueList) {
+    		sum += num;
+    	}
+    	
+    	return sum;
+    }
+}
---
+++ src/codingTest/run.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+public class run {
+	public static void main(String[] args) {
+		
+		KAKAO2022TECHINTERNSHIP2 test = new KAKAO2022TECHINTERNSHIP2();
+		
+		int[] q1 = {3, 2, 7, 2};
+		int[] q2 = {4, 6, 5, 1};
+	
+		System.out.print(test.solution(q1, q2));
+	}
+}
